cac47eca91b852d26179a1f82e4c2da8d52f2e91
Penina Waithaka <peninawaithaka5@gmail.com>
2025-01-16 19:02:07 +0300

    updated the login test

--- features/environment.py
+++ features/environment.py
@@ -1,15 +1,14 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+from dotenv import load_dotenv
+import os
 
 def before_all(context):
-    # chrome_options = Options() #--headless - runs tests in a headless mode
-
-    # service = Service("D:\python\selenium\chrome.exe")
-    #context.driver = webdriver.Chrome(service=service, options=chrome_options)
     context.driver = webdriver.Chrome()
-    context.driver.implicitly_wait(20)
+    context.driver.implicitly_wait(30) # Waits for 10 seconds before throwing NoSuchElementException - globally defined
 
+    load_dotenv()
+    context.username = os.getenv("TEST_USERNAME")
+    context.password = os.getenv("TEST_PASSWORD")
+    
 def after_all(context):
     context.driver.quit()
--- features/steps/login.py
+++ features/steps/login.py
@@ -3,26 +3,60 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 @step('the user navigates to the login page')
 def step_navigate_to_login_page(context):
     context.driver.get('https://www.demoblaze.com/')
-    context.driver.implicitly_wait(30)
 
     #print(context.driver.title)
 
-    assert "STORE" in context.driver.title
+    assert "STORE" in context.driver.title, "Title is not visible"
 
 @step('the user enters valid credentials')
 def step_user_enters_valid_credentials(context):
     login_link = context.driver.find_element(By.ID, 'login2')
+    assert login_link.is_displayed(), "Login Link is not visible"
     
+    #mouse movements
     actions = ActionChains(context.driver)
 
-    actions.move_to_element(login_link).click()
-    context.driver.implicitly_wait(60)
+    actions.move_to_element(login_link).perform()
+    login_link.click()
+  
+    wait = WebDriverWait(context.driver, 30)
+    login_form = wait.until(EC.visibility_of_element_located((By.ID, 'logInModalLabel')))
+    #context.driver.find_element(By.ID, 'logInModalLabel')
+    assert login_form.is_displayed(), "Login Form is not displayed"
+    print("assertions")
 
-    print("Are we getting to this point")
+
+    #filling the form
+    user_name = context.driver.find_element(By.ID, 'loginusername')
+    assert user_name.is_displayed(), "Username is not visible"
+    user_name.clear()
+    user_name.send_keys(context.username)
+
+
+    password = context.driver.find_element(By.ID, 'loginpassword')
+    assert password.is_displayed(), "Password is not displayed"
+    password.clear()
+    password.send_keys(context.password)
+
+    #asserting that the close and login buttons are visible
+    # buttons = context.driver.find_elements(By.TAG_NAME, 'button')
+    # buttons[0].click()
+
+    close = context.driver.find_element(By.XPATH, "//button[normalize-space(@class)='btn btn-primary']")
+    print(close.is_displayed())
+
+    assert close.is_displayed(), "Close Button is not visible"
+
+    login = context.driver.find_element(By.CLASS_NAME, 'btn.btn-primary')
+    assert login.is_displayed(), "Login Button is not visible"
+
+    
 
 @step('the user is directed to their logged in home page')
 def step_user_directed_to_home_page(context):
